eb99c8edd3321efb473fae3a92091206b06b3f1f
Valera Makarov <valmackaroff@yandex.ru>
2025-05-06 21:33:13 +0300

    Обновление

---
+++ data/data.py
@@ -0,0 +1,11 @@
+order_data = {
+    "firstName": "Валерий",
+    "lastName": "Петров",
+    "address": "Москва, ул. Тверская, д.5",
+    "metroStation": 4,
+    "phone": "+79991112233",
+    "rentTime": 5,
+    "deliveryDate": "2024-07-01",
+    "comment": "Побыстрее, пожалуйста",
+    "color": ["BLACK"]
+}
--- helpers/api.py
+++
@@ -1,8 +0,0 @@
-import requests
-#  Макаров Валерий, 29-я когорта — Финальный проект. Инженер по тестированию плюс
-
-class ScooterAPI:
-    BASE_URL = "https://0834152a-9c57-4c5a-bc9d-43fa46bc7e34.serverhub.praktikum-services.ru/api/v1"
-
-    def create_order(self, order_data):
-        return requests.post(f"{self.BASE_URL}/orders", json=order_data)
